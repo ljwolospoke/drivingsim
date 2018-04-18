79594fc42d33db40a002d08afffbf348702a8c2e
bdarbog <bdarbog@bdarbog.it.pointpark.edu>
2018-04-17 21:14:10 -0400

    with timer

--- drivingsim.js
+++ drivingsim.js
@@ -11,7 +11,6 @@ var carsimulator = (function() {
     //carY += 1;
     //notify();
   //}, 35);
-
   // store all the listeners
   // we have to call all of them if something changes
   var listeners = [];
@@ -39,3 +38,26 @@ var carsimulator = (function() {
 
 
 })();
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)//change to 10
+        seconds = parseInt(timer % 60, 10);//to go down from 1 minute
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinutes = 60 * 1,//start at 1 minute
+        display = document.querySelector('#time');
+    startTimer(oneMinutes, display);
+
+};
