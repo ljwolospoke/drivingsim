8cea6fcd9061651de39ad2190064fc2d503f79af
bdarbog <bdarbog@bdarbog.it.pointpark.edu>
2018-04-17 20:26:51 -0400

    js + html

--- drivingsim.js
+++ drivingsim.js
@@ -1,29 +1,4 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)//change to 10
-        seconds = parseInt(timer % 60, 10);//to go down from 1 minute
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var oneMinutes = 60 * 1,//start at 1 minute
-        display = document.querySelector('#time');
-    startTimer(oneMinutes, display);
-
-};
-
-
-var drivingsim = (function() {
+var carsimulator = (function() {
 
   // this is the internal state of the model
   // no one can access this directly
